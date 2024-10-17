ec815e865305f1603544c7a7dc247256b289262f
IIE-Bonny <ST10435674@rcconnect.edu.za>
2024-10-17 08:37:05 +0200

    Task class and declarations commit

---
+++ src/main/java/com/mycompany/bonnypoe/part2/Task.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.bonnypoe.part2;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author B.O.N.N.Y
+ */
+public class Task {
+    //Declaration of task variables
+    private String taskName;
+    private int taskNumber;
+    private String taskDescription;
+    private String developerDetails;
+    private int taskDuration;
+    private String taskID;
+    private String taskStatus;
+}
