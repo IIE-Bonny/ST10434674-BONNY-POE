8bb25ae57a263acd1e69ccdd951787b46e65db6f
IIE-Bonny <ST10435674@rcconnect.edu.za>
2024-10-19 14:24:59 +0200

    Switch case and task generator commit

--- src/main/java/com/mycompany/bonnypoe/part2/BONNYPOEPART2.java
+++ src/main/java/com/mycompany/bonnypoe/part2/BONNYPOEPART2.java
@@ -6,6 +6,7 @@ package com.mycompany.bonnypoe.part2;
 
 import java.util.Scanner;
 import java.util.regex.Pattern;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -17,6 +18,9 @@ public class BONNYPOEPART2 {
      
          //Login instance
         Login login_checks = new Login();
+        
+        
+        
            
            // object for a scanner
         Scanner sc = new Scanner(System.in);
@@ -48,6 +52,63 @@ public class BONNYPOEPART2 {
             password = sc.nextLine();
         }while(!login_checks.checkPasswordComplexity(password));
         
+       //Display Welcome message
+       JOptionPane.showMessageDialog(null, "Welcome to EasyKanban!", "EasyKanban", JOptionPane.INFORMATION_MESSAGE);
        
+        String booleanString = login_checks.returnLoginStatus(true);
+       //Use of Boolean.parseBoolean() to convert thestring to a boolean
+       //The parseBoolean() method checks the string and converts it to a boolean.
+       //If the string is exactly "true" (ignoring case), it return true.
+        boolean convertedBoolean = Boolean.parseBoolean(booleanString);
+       
+       //Use the converted boolean in an if statement
+       //This checks the value of the converted boolean and executes
+       //Display menu after user has logged in
+       if(convertedBoolean){
+           //Main loop to keep the application running until the user quits
+           while(convertedBoolean){
+        String[] option = {"Add Tasks","Show Report(Coming Soon)","Quit"};
+        int choice = JOptionPane.showOptionDialog(null, "Choose an option", "Menu", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
+        
+        switch(choice){
+            case 0:
+                //User selected "Add Task"
+                addTask();
+                break;
+            case 1:
+                //User selected "Show Report",which is not yet available
+                JOptionPane.showMessageDialog(null, "Coming Soon", "Report", JOptionPane.INFORMATION_MESSAGE);
+                break;
+            case 2:
+                //User selected "Quit", exit the loop
+                convertedBoolean = false;
+                JOptionPane.showMessageDialog(null, "Exiting EasyKanban\n Goodbye", "Exit", JOptionPane.INFORMATION_MESSAGE);
+                
+        }
+    }
+       }
+       //Loop to gather details for each task
+       for(int i = 0; i <taskCount; i++){
+           //Get task name
+           String taskName = JOptionPane.showInputDialog("Enter Task Name " +(i+1)+ ":");
+           
+           //Get taskDescription and ensure its not longer than 50 characters
+           String taskDescription = JOptionPane.showInputDialog("Enter Task Description(max 50 characters):");
+           if(taskDescription.length() > 50){
+               JOptionPane.showMessageDialog(null, "Description too long.", "Warning", JOptionPane.WARNING_MESSAGE);
+               taskDescription = taskDescription.substring(0, 50);
+           }
+           //Get developer details
+           String developerDetails = JOptionPane.showInputDialog("Enter Developer's Frst and Last Name: ");
+           
+           //Get task Duration and ensure its a valid number
+           String taskDurationStr = JOptionPane.showInputDialog("Enter Task Duration(in hours): ");
+           //Convert to integer
+           int taskDuration = Integer.parseInt(taskDurationStr);
+           
+           //Ask user to choose task status from a list
+           
+           
+       }
     }
 }
--- src/main/java/com/mycompany/bonnypoe/part2/Task.java
+++ src/main/java/com/mycompany/bonnypoe/part2/Task.java
@@ -5,7 +5,7 @@
 package com.mycompany.bonnypoe.part2;
 import java.util.ArrayList;
 import java.util.Scanner;
-import javax.swing.JOptionPane;
+import javax.swing.*;
 
 /**
  *
@@ -21,7 +21,7 @@ public class Task {
     private String taskID;
     private String taskStatus;
     
-    
+    //Constructor to initialize all task details and generate the task ID
     public Task(String taskName, int taskNumber, String taskDescription,String developerDetails, int taskDuration, String taskStatus){
         //Assigning of variables
         this.taskName = taskName;
@@ -32,7 +32,31 @@ public class Task {
         this.taskStatus = taskStatus;
         this.taskID = generateTaskID();
     }
+    //Generates Task ID in the formate of-
+    //first 2 letters of task name:task number:last 3 letters of the developer
     private String generateTaskID(){
-       
+       String taskNamePart;
+       String developerPart;
+       if(taskName.length() >= 2){
+           taskNamePart = taskName.substring(0,2).toUpperCase();
+       }else{
+           taskNamePart = taskName.toUpperCase();
+       }
+       if(developerDetails.length() >= 3){
+           developerPart = developerDetails.substring(developerDetails.length() - 3).toUpperCase();
+       }else{
+           developerPart = developerDetails.toUpperCase();
+       }
+        return taskNamePart + ":" + taskNumber + ":" + developerPart;
+    }
+    //Returns the task information as a formatted string
+    @Override
+    public String toString(){
+        return "Task ID:" + taskID + "\nTask Name:" 
+                + taskName + "\nTaskNumber: " 
+                + taskNumber +"\nTaskDescription: " 
+                + taskDescription + "\nDeveloper: " +developerDetails 
+                + "\nDuration: " + taskDuration + "\nhours\nstatus: " + taskStatus;
+        
     }
 }
